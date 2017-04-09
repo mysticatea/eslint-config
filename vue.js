d7882a074cc449690120f416a4a0e81f3339fab2
Toru Nagashima <star.ctor@gmail.com>
2017-04-09 14:25:07 +0900

    Breaking: change 'mysticatea/vue' config to ES modules based

--- vue.js
+++ vue.js
@@ -6,20 +6,29 @@
 "use strict"
 
 module.exports = {
-    "extends": ["./browser.js", "./node.js"],
+    "extends": ["./browser.js", "./modules.js"],
+    "globals": {"process": false},
     "parser": "vue-eslint-parser",
     "parserOptions": {
         "ecmaFeatures": {"jsx": true},
         "ecmaVersion": 2017,
     },
-    "plugins": ["react"],
+    "plugins": ["node", "react"],
     "rules": {
-        "node/no-unsupported-features": ["error", {"version": 7.6}],
+        "no-process-env": "off",
+        "node/no-deprecated-api": "error",
+        "node/no-missing-import": "error",
+        "node/no-missing-require": "error",
+        "node/no-unpublished-bin": "error",
+        "node/no-unpublished-import": "error",
+        "node/no-unpublished-require": "error",
+        "node/process-exit-as-throw": "error",
+        "node/shebang": "error",
         "react/jsx-uses-react": "error",
         "react/jsx-uses-vars": "error",
     },
     "settings": {
-        "node": {"tryExtensions": [".js", ".vue"]},
+        "node": {"tryExtensions": [".js", ".json", ".vue"]},
         "react": {"pragma": "h"},
     },
 }
